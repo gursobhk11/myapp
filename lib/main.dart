d06f0a208023f202f6f9449d9ed7a1db8edee8c6
21gursobhkaur <21gursobhkaur@gmail.com>
2025-04-09 21:24:44 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/home_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   //Initialize Flutter bindings
